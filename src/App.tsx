3f00b9f36b7d4b08c584023a0d0e9421bc3914d3
rubuz <rubuz23@gmail.com>
2024-06-14 13:43:01 +0200

    commit init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Button>TEST</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
